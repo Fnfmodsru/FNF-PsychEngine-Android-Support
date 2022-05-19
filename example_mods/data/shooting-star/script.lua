a7b8b53a7f04507d7a99617e9dcff9d62335db1f
Fnfmodsru <98064212+Fnfmodsru@users.noreply.github.com>
2022-05-19 12:18:50 +0900

    Delete example_mods/data directory

--- example_mods/data/shooting-star/script.lua
+++
@@ -1,9 +0,0 @@
-function onCreate()
-	setPropertyFromClass('GameOverSubstate', 'characterName', 'panchitobf-ded'); --Character json file for the death animation
-	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'revenge_vf_die'); --put in mods/sounds/
-	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver'); --put in mods/music/
-	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd'); --put in mods/music/
-makeLuaText('watermark', songName .. " | Created By Iamsii  ", 0, -2, 550); -- You can edit the created by Prisma Text just dont change anything else
-    setTextSize('watermark', 15);
-    addLuaText('watermark');
-end
